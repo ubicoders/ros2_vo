a8323efafaf2d4c1a01fc08e419badb0ab75d094
ubicoders <ubicoders.0@gmail.com>
2025-11-26 21:42:57 -0800

    udpated

--- robot_ws/src/ubicoders_svo/executables/svo_main.cpp
+++ robot_ws/src/ubicoders_svo/executables/svo_main.cpp
@@ -3,6 +3,8 @@
 int32_t main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   const rclcpp::NodeOptions options;
+
+  // ros2 node
   auto basic_vo_node = std::make_shared<UbiSVO::StereoVisualOdometry>(options);
 
   rclcpp::spin(basic_vo_node->get_node_base_interface());
